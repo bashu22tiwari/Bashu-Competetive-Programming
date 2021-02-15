d23547331972ecfc96694f4dab95a886d23061d2
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-02-15 19:39:06 +0530

    Codeforces Questions.

--- abc.cpp
+++ abc.cpp
@@ -1,55 +1,52 @@
-#include <bits/stdc++.h>
-#include <iostream>
+// C++ program to convert 12 hour to 24 hour
+// format
+#include<iostream>
 using namespace std;
-
+ 
+void print24(string str)
+{
+    
+    int h1 = (int)str[1] - '0';
+    int h2 = (int)str[0] - '0';
+    int hh = (h2 * 10 + h1 % 10);
+ 
+    if (str[6] == 'A')
+    {
+        if (hh == 12)
+        {
+            cout << "00";
+            for (int i=2; i <= 5; i++)
+                cout << str[i];
+        }
+        else
+        {
+            for (int i=0; i <= 5; i++)
+                cout << str[i];
+        }
+    }
+ 
+    
+    else
+    {
+        if (hh == 12)
+        {
+            cout << "12";
+            for (int i=2; i <= 5; i++)
+                cout << str[i];
+        }
+        else
+        {
+            hh = hh + 12;
+            cout << hh;
+            for (int i=2; i <= 5; i++)
+                cout << str[i];
+        }
+    }
+}
+ 
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
-    {
-       int n;
-       int odd=0, even=0;
-       cin >> n ;
-       int array[n];
-       for(int i=0; i<n ; i++)
-       {
-           cin >> array[i];
-       }
-       for(int i=0; i<n ; i++)
-       {
-           if(array[i]%2!=0)
-           {
-               odd++;
-           }
-           else
-           {
-               even++;
-           }
-       }
-
-       if(odd==n)
-       {
-           cout << "0" << "\n" ;
-       }
-       else if(odd>=0 && odd!=n && odd<even)
-       {
-           cout << odd << "\n";
-       }
-       else if(even>=0 && even!=n && even<odd)
-       {
-           cout << even << "\n";
-       }
-       else if(even==odd)
-       {
-           cout << even << "\n";
-       }
-       else
-       {
-           cout << "0" << "\n";
-       }
-       
-        
-    }
-    return 0;
+   string str = "12:00 AM";
+   print24(str);
+   return 0;
 }
